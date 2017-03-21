7c2a9017f73040ad7178b669a02b278d0c45d5e5
drefk99 <drefk94@gmail.com>
2017-03-21 18:36:58 -0500

    Añadir ruta absoluta para el cron

--- python-scripts-historico/auto_todo.sh
+++ python-scripts-historico/auto_todo.sh
@@ -2,9 +2,12 @@
 # -*- ENCODING: UTF-8 -*-
 
 
+#Añadir la ruta absoluta a env para ejecutar con el cron
 
 source env/bin/activate
 yest_man=`expr $(date +%d) + 1`
+
+#Añadir la ruta absoluta a charls.py para ejecutar con el cron
 
 python charls.py bancomer $(date +%Y-%m-%d) $(date +%Y-%m-$yest_man)
 #python charls.py bancomer 2017-03-16 2017-03-17
