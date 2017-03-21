132317203c7c272ae981e44324d248a0308bc636
drefk99 <drefk94@gmail.com>
2017-03-21 18:31:00 -0500

    Retiro de rutas absolutas

--- python-scripts-historico/auto_todo.sh
+++ python-scripts-historico/auto_todo.sh
@@ -3,8 +3,8 @@
 
 
 
-source /home/administradorcito/Documentos/data-storage/env/bin/activate
+source env/bin/activate
 yest_man=`expr $(date +%d) + 1`
 
-python /home/administradorcito/Documentos/data-storage/proyecto1/charls.py bancomer $(date +%Y-%m-%d) $(date +%Y-%m-$yest_man)
+python charls.py bancomer $(date +%Y-%m-%d) $(date +%Y-%m-$yest_man)
 #python charls.py bancomer 2017-03-16 2017-03-17
