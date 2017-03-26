87adfeda508294c0932fa66240c1965ed6d431ba
drefk99 <drefk94@gmail.com>
2017-03-26 06:46:12 -0600

    Actualizacion del .py

--- python-scripts-historico/auto_todo.sh
+++ python-scripts-historico/auto_todo.sh
@@ -9,5 +9,5 @@ yest_man=`expr $(date +%d) + 1`
 
 #Añadir la ruta absoluta a charls.py para ejecutar con el cron
 
-python charls.py bancomer $(date +%Y-%m-%d) $(date +%Y-%m-$yest_man)
+python his_bancomer.py bancomer $(date +%Y-%m-%d) $(date +%Y-%m-$yest_man)
 #python charls.py bancomer 2017-03-16 2017-03-17
